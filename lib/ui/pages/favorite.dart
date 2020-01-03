161849c2042af32117369226f3788cd72d942287
Marcos Silva Santos <marcos.silva.santos@accenture.com>
2020-01-03 19:03:10 -0300

    Página Favoritos: Estrutura

--- lib/ui/pages/favorite.dart
+++ lib/ui/pages/favorite.dart
@@ -1,10 +1,28 @@
 import 'package:flutter/material.dart';
+import 'package:the_movie_database/ui/widgets/cardList.dart';
 
 class Favorite extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Container(
-      child: Text('Favorite'),
+    final List<String> entries = <String>['A', 'B', 'C', 'A', 'B', 'C'];
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Favorites'),
+      ),
+      body: ListView.separated(
+        padding: EdgeInsets.only(top: 10.0),
+        itemCount: entries.length,
+        itemBuilder: (BuildContext context, int index) {
+          return CardList();
+        },
+        separatorBuilder: (
+          BuildContext context,
+          int index,
+        ) =>
+            const Divider(
+          color: Colors.white,
+        ),
+      ),
     );
   }
 }
