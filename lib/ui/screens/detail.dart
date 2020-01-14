59b01db82cebdf575927a1cdfa45e0974e834aec
Marcos Silva Santos <marcos.silva.santos@accenture.com>
2020-01-14 21:21:40 -0200

    Tela de detalhes

--- lib/ui/screens/detail.dart
+++ lib/ui/screens/detail.dart
@@ -15,84 +15,65 @@ class Detail extends StatelessWidget {
     final sizeTitleSubSection = 20.0;
     return Scaffold(
       body: SafeArea(
-        top: false,
-        bottom: false,
-        child: NestedScrollView(
-          headerSliverBuilder: (
-            BuildContext context,
-            bool innerBoxIsScrolled,
-          ) {
-            return <Widget>[
-              SliverAppBar(
-                // title: Text('Star Wars: The Rise of Skywalker'),
-                expandedHeight: 200.0,
-                floating: false,
-                pinned: true,
-                elevation: 0.0,
-                flexibleSpace: FlexibleSpaceBar(
-                  background: FadeInImage.memoryNetwork(
-                    placeholder: kTransparentImage,
-                    fit: BoxFit.fitWidth,
-                    image:
-                        'https://image.tmdb.org/t/p/w780/jOzrELAzFxtMx2I4uDGHOotdfsS.jpg',
-                  ),
-                ),
-              ),
-            ];
-          },
-          body: ListView(
+        child: SingleChildScrollView(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.start,
             children: <Widget>[
-              Container(
-                child: Padding(
-                  padding: EdgeInsets.only(
-                    left: 20.0,
-                    right: 20.0,
-                  ),
-                  child: Row(
-                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                    crossAxisAlignment: CrossAxisAlignment.center,
-                    children: <Widget>[
-                      Column(
-                        mainAxisAlignment: MainAxisAlignment.start,
-                        crossAxisAlignment: CrossAxisAlignment.start,
-                        children: <Widget>[
-                          Text(
-                            'Star Wars: The Rise of Skywalker',
-                            overflow: TextOverflow.ellipsis,
-                            maxLines: 2,
-                            style: TextStyle(
-                              color: Colors.white,
-                              fontSize: 20.0,
-                              fontWeight: FontWeight.w900,
-                            ),
-                          ),
-                          Text(
-                            'Action | Adventure | Science Fiction',
-                            style: TextStyle(
-                              color: Colors.white,
-                              fontSize: 14.0,
-                              fontWeight: FontWeight.w400,
-                            ),
-                          ),
-                        ],
-                      ),
-                      Icon(
-                        Icons.favorite,
-                        color: Colors.red,
-                      ),
-                    ],
-                  ),
-                ),
+              _buildStack(fullSize),
+              _overview(),
+              SizedBox(
+                height: 20.0,
               ),
               Container(
-                padding: EdgeInsets.all(20.0),
-                child: Text(
-                  'The surviving Resistance faces the First Order once again as the journey of Rey, Finn and Poe Dameron continues. With the power and knowledge of generations behind them, the final battle begins.',
-                  style: TextStyle(
-                    color: Colors.white,
-                    fontSize: 16.0,
+                width: double.infinity,
+                padding: EdgeInsets.symmetric(horizontal: 20.0),
+                decoration: BoxDecoration(
+                  color: Colors.white,
+                  borderRadius: BorderRadius.all(
+                    Radius.circular(20.0),
                   ),
                 ),
+                child: Column(
+                  children: <Widget>[
+                    _setTitleSection(
+                      text: 'Recommendations',
+                      size: sizeTitleSubSection,
+                    ),
+                    Container(
+                      margin: EdgeInsets.only(
+                        bottom: 15.0,
+                        left: 5.0,
+                      ),
+                      height: posterHeight,
+                      child: ListView.builder(
+                        itemExtent: posterWidth,
+                        scrollDirection: Axis.horizontal,
+                        itemBuilder: (context, index) => Text('POSTER'),
+                        itemCount: 20,
+                      ),
+                    ),
+                    _setTitleSection(
+                      text: 'Recommendations',
+                      size: sizeTitleSubSection,
+                    ),
+                    Container(
+                      margin: EdgeInsets.only(
+                        bottom: 15.0,
+                        left: 5.0,
+                      ),
+                      height: posterHeight,
+                      child: ListView.builder(
+                        itemExtent: posterWidth,
+                        scrollDirection: Axis.horizontal,
+                        itemBuilder: (context, index) => Text('POSTER'),
+                        itemCount: 20,
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+              SizedBox(
+                height: 20.0,
               ),
             ],
           ),
@@ -100,4 +81,101 @@ class Detail extends StatelessWidget {
       ),
     );
   }
+
+  Container _overview() => Container(
+        padding: EdgeInsets.all(20.0),
+        child: Text(
+          'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industrys standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.',
+          style: TextStyle(
+            color: Colors.white,
+            fontSize: 16.0,
+          ),
+        ),
+      );
+
+  Widget _buildStack(double fullSize) => Stack(
+        children: <Widget>[
+          Container(
+            width: double.infinity,
+            height: 352.0,
+            padding: EdgeInsets.only(bottom: 120.0),
+            child: FadeInImage.memoryNetwork(
+              placeholder: kTransparentImage,
+              fit: BoxFit.fitWidth,
+              image:
+                  'https://image.tmdb.org/t/p/w780/a6cDxdwaQIFjSkXf7uskg78ZyTq.jpg',
+            ),
+          ),
+          Positioned(
+            bottom: 0.0,
+            left: 20.0,
+            child: Row(
+              children: <Widget>[
+                Container(
+                  width: 120.0,
+                  child: ClipRRect(
+                    borderRadius: BorderRadius.circular(8.0),
+                    child: FadeInImage.memoryNetwork(
+                      placeholder: kTransparentImage,
+                      fit: BoxFit.fitWidth,
+                      image:
+                          'https://image.tmdb.org/t/p/w154/or06FN3Dka5tukK1e9sl16pB3iy.jpg',
+                    ),
+                  ),
+                ),
+                Container(
+                  width: fullSize - 120.0,
+                  margin: EdgeInsets.only(top: 60.0),
+                  child: Padding(
+                    padding: EdgeInsets.all(20.0),
+                    child: Column(
+                      children: <Widget>[
+                        Text(
+                          'Terminator: Dark Fate Terminator Terminator: Dark Fate Terminator Dark Fate Terminator',
+                          overflow: TextOverflow.ellipsis,
+                          maxLines: 2,
+                          style: TextStyle(
+                            color: Colors.white,
+                            fontSize: 20.0,
+                            fontWeight: FontWeight.w900,
+                          ),
+                        ),
+                        Text(
+                          'Terminator | Dark Fate | Terminator | Terminator | Dark Fate | Terminator | Dark Fate | Terminator',
+                          overflow: TextOverflow.ellipsis,
+                          maxLines: 2,
+                          style: TextStyle(
+                            color: Colors.white,
+                            fontSize: 14.0,
+                            fontWeight: FontWeight.w400,
+                          ),
+                        ),
+                      ],
+                    ),
+                  ),
+                ),
+              ],
+            ),
+          ),
+        ],
+      );
+
+  Widget _setTitleSection({
+    String text,
+    double size,
+  }) =>
+      ListTile(
+        title: Row(
+          children: <Widget>[
+            Text(
+              text,
+              style: TextStyle(
+                color: Colors.black,
+                fontSize: size,
+                fontWeight: FontWeight.w500,
+              ),
+            ),
+          ],
+        ),
+      );
 }
